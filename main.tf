3d5b4eb2d4e9dca64611f391a266950062e5b732
Victor Hernandez <binario200@gmail.com>
2020-06-29 19:06:47 +0000

    adding random generation to concat to storage account anme

--- main.tf
+++ main.tf
@@ -4,8 +4,16 @@ resource "azurerm_resource_group" "lab2" {
   tags     = var.tags
 }
 
+resource "random_string" "rnd" {
+  length  = 8
+  lower   = true
+  number  = true
+  upper   = false
+  special = false
+}
+
 resource "azurerm_storage_account" "lab2sa" {
-  name                      = "${azurerm_resource_group.lab2.tags["source"]}binatflab2"
+  name                      = "sa${var.tags["source"]}${random_string.rnd.result}"
   resource_group_name       = azurerm_resource_group.lab2.name
   location                  = azurerm_resource_group.lab2.location
   tags                      = azurerm_resource_group.lab2.tags
